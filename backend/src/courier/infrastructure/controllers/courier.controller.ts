bf7eb41677fce92abb85d34df765abe1c75faa01
Jhair7474 <marlonmoncayo7474@gmail.com>
2026-05-24 16:39:06 -0500

    Arreglo de error en controlador de courier con el Jwt

--- backend/src/courier/infrastructure/controllers/courier.controller.ts
+++ backend/src/courier/infrastructure/controllers/courier.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get, Param, UseGuards } from '@nestjs/common';
 import { ApiTags, ApiOperation, ApiBearerAuth } from '@nestjs/swagger';
-import { AuthGuard } from '@nestjs/passport';
 import { GetCourierProfileUseCase } from '../../application/use-cases/get-courier-profile.use-case';
+import { JwtAuthGuard } from 'src/auth/infrastructure/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/auth/infrastructure/guards/roles.guard';
+import { Roles } from 'src/auth/infrastructure/decorators/roles.decorator';
 
 @ApiTags('Couriers')
 @ApiBearerAuth()
 @Controller('couriers')
-@UseGuards(AuthGuard('jwt'))
+@UseGuards(JwtAuthGuard, RolesGuard)
 export class CourierController {
   constructor(
     private readonly getCourierProfileUseCase: GetCourierProfileUseCase,
